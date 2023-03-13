07c4034bf0e9515807ee957888d63b4a59677aa7
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-03-13 12:25:26 -0700

    updates lab code

--- index.js
+++ index.js
@@ -4,19 +4,49 @@ const headquarters = 42;
 const convertBlockToFeet = 264;
 
 function distanceFromHqInBlocks(distance) {
+    return Math.abs(distance - headquarters);
+}
+
+
+function distanceFromHqInFeet(distance) {
+    distanceFromHqInBlocks(distance);
+    return distanceFromHqInBlocks(distance) * convertBlockToFeet; 
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * convertBlockToFeet;
+}
+
+
+function calculatesFarePrice(start, destination) {
+    let totalTravel = (Math.abs(start - destination) * convertBlockToFeet);
+    if (totalTravel <= 400){
+        return 0;
+    } else if (totalTravel > 400 && totalTravel < 2000) {
+        return (totalTravel - 400) * 0.02;
+    } else if ( totalTravel > 2000 && totalTravel < 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }  
+}
+
+
+
+/*
+function distanceFromHqInBlocks(distance) {
     if (distance >= 42) {
         return distance - headquarters;
     } else if (distance < 42) {
         return headquarters - distance;
     }
 }
+*/
 
-function distanceFromHqInFeet(distance) {
-    distanceFromHqInBlocks(distance);
-    return distanceFromHqInBlocks(distance) * convertBlockToFeet;
-    
-}
 
+
+/*
 function distanceTravelledInFeet(start, destination) {
     if((start - destination) <= 0) {
         return (destination - start) * convertBlockToFeet;
@@ -24,7 +54,12 @@ function distanceTravelledInFeet(start, destination) {
         return (start - destination) * convertBlockToFeet;
     }
 }
+*/
 
+
+
+
+/*
 function calculatesFarePrice(start, destination) {
     if((Math.abs(start - destination) * convertBlockToFeet) <= 400) {
         return 0;
@@ -38,4 +73,4 @@ function calculatesFarePrice(start, destination) {
         return "cannot travel that far";
     }
 }
-
+*/
